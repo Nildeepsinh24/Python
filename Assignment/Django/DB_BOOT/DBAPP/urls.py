04140ae601e10cc9ce2e4a9bdf9692b15270f93e
Nildeepsinh24 <neeldeepsinhjadeja24@gmail.com>
2025-10-04 19:08:47 +0530

    Database connectivity Project

---
+++ Assignment/Django/DB_BOOT/DBAPP/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from DBAPP import views
+
+urlpatterns = [
+    path('', views.index),
+    path("showdata/",views.showdata,name='showdata'),
+    path("updatedata/<int:id>",views.updatedata),
+    path("deletedata/<int:id>",views.deletedata),
+    
+]
---
+++ Assignment/Django/DB_BOOT/DBAPP/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+
+from DBAPP.forms import StudInfoForm
+from DBAPP.models import Studinfo
+from .forms import *
+
+# Create your views here.
+def index(request):
+    if request.method=='POST': #TRUE
+        form=StudInfoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("Record Inserted!")
+        else:
+            print(form.errors)
+    return render(request,'index.html')
+
+def showdata(request):
+    stdata=Studinfo.objects.all()
+    print(stdata)
+    return render(request,'showdata.html',{'stdata':stdata})
+
+def updatedata(request,id):
+    stid=Studinfo.objects.get(id=id)
+    if request.method=='POST': #TRUE
+        form=StudInfoForm(request.POST,instance=stid)
+        if form.is_valid():
+            form.save()
+            print("Record Updated!")
+            return redirect('showdata')
+        else:
+            print(form.errors)
+    return render(request,'updatedata.html',{'stid':stid})
+
+def deletedata(request,id):
+    stid=Studinfo.objects.get(id=id)
+    Studinfo.delete(stid)
+    return redirect('showdata')
+
